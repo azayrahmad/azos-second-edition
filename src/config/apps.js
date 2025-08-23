1594fc41ea6075e0561853c3ae6578860642b91e
Aziz Rahmad <azayrahmad@gmail.com>
2025-08-24 01:48:31 +0800

    simplify apps

--- src/config/apps.js
+++ src/config/apps.js
@@ -2,7 +2,7 @@ export const apps = [
   {
     id: "about",
     title: "About",
-    icon: new URL('../assets/icons/SHELL32_3.ico', import.meta.url).href,
+    icon: new URL('../assets/icons/COMCTL32_20481.ico', import.meta.url).href,
     path: "/about/",
     action: {
       type: "window",
@@ -34,78 +34,78 @@ export const apps = [
       },
     },
   },
-  {
-    id: "notepad",
-    title: "Notepad",
-    icon: "./src/assets/icons/notepad.ico",
-    path: "/notepad/",
-    action: {
-      type: "window",
-      window: {
-        width: 500,
-        height: 400,
-        resizable: true,
-        menuBar: {
-          File: [
-            {
-              label: "&New",
-              shortcutLabel: "Ctrl+N",
-              action: () => console.log("New document"),
-            },
-            {
-              label: "&Save",
-              shortcutLabel: "Ctrl+S",
-              action: () => console.log("Save document"),
-            },
-            { label: "-" },
-            {
-              label: "E&xit",
-              action: (win) => win.close(),
-            },
-          ],
-          Edit: [
-            {
-              label: "&Undo",
-              shortcutLabel: "Ctrl+Z",
-              enabled: false,
-            },
-            { label: "-" },
-            {
-              label: "Cu&t",
-              shortcutLabel: "Ctrl+X",
-            },
-            {
-              label: "&Copy",
-              shortcutLabel: "Ctrl+C",
-            },
-            {
-              label: "&Paste",
-              shortcutLabel: "Ctrl+V",
-            },
-          ],
-        },
-        content: `
-          <div class="notepad-content" style="padding: 8px;">
-            <textarea style="width: 100%; height: calc(100% - 16px); resize: none;"></textarea>
-          </div>
-        `,
-      },
-    },
-  },
-  {
-    id: "shutdown",
-    title: "Shut Down",
-    icon: "./src/assets/icons/shutdown.ico",
-    action: {
-      type: "function",
-      handler: () => {
-        if (confirm("Are you sure you want to shut down the system?")) {
-          document.body.innerHTML =
-            '<div style="text-align: center; padding-top: 40vh;">It is now safe to turn off your computer.</div>';
-        }
-      },
-    },
-  },
+  // {
+  //   id: "notepad",
+  //   title: "Notepad",
+  //   icon: "./src/assets/icons/notepad.ico",
+  //   path: "/notepad/",
+  //   action: {
+  //     type: "window",
+  //     window: {
+  //       width: 500,
+  //       height: 400,
+  //       resizable: true,
+  //       menuBar: {
+  //         File: [
+  //           {
+  //             label: "&New",
+  //             shortcutLabel: "Ctrl+N",
+  //             action: () => console.log("New document"),
+  //           },
+  //           {
+  //             label: "&Save",
+  //             shortcutLabel: "Ctrl+S",
+  //             action: () => console.log("Save document"),
+  //           },
+  //           { label: "-" },
+  //           {
+  //             label: "E&xit",
+  //             action: (win) => win.close(),
+  //           },
+  //         ],
+  //         Edit: [
+  //           {
+  //             label: "&Undo",
+  //             shortcutLabel: "Ctrl+Z",
+  //             enabled: false,
+  //           },
+  //           { label: "-" },
+  //           {
+  //             label: "Cu&t",
+  //             shortcutLabel: "Ctrl+X",
+  //           },
+  //           {
+  //             label: "&Copy",
+  //             shortcutLabel: "Ctrl+C",
+  //           },
+  //           {
+  //             label: "&Paste",
+  //             shortcutLabel: "Ctrl+V",
+  //           },
+  //         ],
+  //       },
+  //       content: `
+  //         <div class="notepad-content" style="padding: 8px;">
+  //           <textarea style="width: 100%; height: calc(100% - 16px); resize: none;"></textarea>
+  //         </div>
+  //       `,
+  //     },
+  //   },
+  // },
+  // {
+  //   id: "shutdown",
+  //   title: "Shut Down",
+  //   icon: "./src/assets/icons/shutdown.ico",
+  //   action: {
+  //     type: "function",
+  //     handler: () => {
+  //       if (confirm("Are you sure you want to shut down the system?")) {
+  //         document.body.innerHTML =
+  //           '<div style="text-align: center; padding-top: 40vh;">It is now safe to turn off your computer.</div>';
+  //       }
+  //     },
+  //   },
+  // },
   {
     id: "clippy",
     title: "Office Assistant",
