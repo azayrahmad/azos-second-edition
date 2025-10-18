88dcdc321382aaf8db28833876bb62da0d16eb08
Aziz Rahmad <azayrahmad@gmail.com>
2025-10-19 00:31:35 +0800

    update configs

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,30 @@
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default {
+export default defineConfig({
     "base": "/azos-second-edition/",
-    plugins: [
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-            manifest: {
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
                 name: 'azOS Second Edition',
                 short_name: 'azOS',
                 description: 'A web-based OS simulation.',
-                theme_color: '#008080',
-                icons: [
-                    {
-                        src: 'pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    }
-                ]
-            }
-        })
-    ]
-}
+        theme_color: '#008080',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
