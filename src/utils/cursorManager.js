3279c1cb9f5cb235a6d254fdf32cf93c4e65cdec
Aziz Rahmad <azayrahmad@gmail.com>
2025-10-27 11:45:54 +0800

    refactor cursormanager

--- src/utils/cursorManager.js
+++ src/utils/cursorManager.js
@@ -1,38 +1,27 @@
-
 import { cursors } from '../config/cursors.js';
 import { applyAniCursor, clearAniCursor } from './aniCursor.js';
 
+function createCursorTheme(themeName, cursorSet) {
+  return {
+    '--cursor-default': { value: `url(${cursorSet.arrow}), auto` },
+    '--cursor-pointer': { value: `url(${cursorSet.arrow}), pointer` },
+    '--cursor-text': { value: `url(${cursorSet.beam}), text` },
+    '--cursor-wait': { value: 'wait', animated: true, type: 'busy' },
+    '--cursor-progress': { value: "progress", animated: true, type: "wait" },
+    '--cursor-help': { value: `url(${cursorSet.help}), help` },
+    '--cursor-move': { value: `url(${cursorSet.move}), move` },
+    '--cursor-not-allowed': { value: `url(${cursorSet.no}), not-allowed` },
+    '--cursor-crosshair': { value: `url(${cursorSet.cross}), crosshair` },
+    '--cursor-nesw-resize': { value: `url(${cursorSet.sizeNESW}), nesw-resize` },
+    '--cursor-ns-resize': { value: `url(${cursorSet.sizeNS}), ns-resize` },
+    '--cursor-nwse-resize': { value: `url(${cursorSet.sizeNWSE}), nwse-resize` },
+    '--cursor-we-resize': { value: `url(${cursorSet.sizeWE}), ew-resize` },
+  };
+}
+
 const cursorThemes = {
-  'dangerous-creatures': {
-    '--cursor-default': { value: `url(${cursors.dangerousCreatures.arrow}), auto` },
-    '--cursor-pointer': { value: `url(${cursors.dangerousCreatures.arrow}), pointer` },
-    '--cursor-text': { value: `url(${cursors.dangerousCreatures.beam}), text` },
-    '--cursor-wait': { value: 'wait', animated: true, type: 'busy' },
-    '--cursor-progress': { value: "progress", animated: true, type: "wait" },
-    '--cursor-help': { value: `url(${cursors.dangerousCreatures.help}), help` },
-    '--cursor-move': { value: `url(${cursors.dangerousCreatures.move}), move` },
-    '--cursor-not-allowed': { value: `url(${cursors.dangerousCreatures.no}), not-allowed` },
-    '--cursor-crosshair': { value: `url(${cursors.dangerousCreatures.cross}), crosshair` },
-    '--cursor-nesw-resize': { value: `url(${cursors.dangerousCreatures.sizeNESW}), nesw-resize` },
-    '--cursor-ns-resize': { value: `url(${cursors.dangerousCreatures.sizeNS}), ns-resize` },
-    '--cursor-nwse-resize': { value: `url(${cursors.dangerousCreatures.sizeNWSE}), nwse-resize` },
-    '--cursor-we-resize': { value: `url(${cursors.dangerousCreatures.sizeWE}), ew-resize` },
-  },
-  '60s-usa': {
-    '--cursor-default': { value: `url(${cursors.usa60s.arrow}), auto` },
-    '--cursor-pointer': { value: `url(${cursors.usa60s.arrow}), pointer` },
-    '--cursor-text': { value: `url(${cursors.usa60s.beam}), text` },
-    '--cursor-wait': { value: 'wait', animated: true, type: 'busy' },
-    '--cursor-progress': { value: "progress", animated: true, type: "wait" },
-    '--cursor-help': { value: `url(${cursors.usa60s.help}), help` },
-    '--cursor-move': { value: `url(${cursors.usa60s.move}), move` },
-    '--cursor-not-allowed': { value: `url(${cursors.usa60s.no}), not-allowed` },
-    '--cursor-crosshair': { value: `url(${cursors.usa60s.cross}), crosshair` },
-    '--cursor-nesw-resize': { value: `url(${cursors.usa60s.sizeNESW}), nesw-resize` },
-    '--cursor-ns-resize': { value: `url(${cursors.usa60s.sizeNS}), ns-resize` },
-    '--cursor-nwse-resize': { value: `url(${cursors.usa60s.sizeNWSE}), nwse-resize` },
-    '--cursor-we-resize': { value: `url(${cursors.usa60s.sizeWE}), ew-resize` },
-  },
+  'dangerous-creatures': createCursorTheme('dangerous-creatures', cursors.dangerousCreatures),
+  '60s-usa': createCursorTheme('60s-usa', cursors.usa60s),
 };
 
 const allCursorProperties = Object.keys(cursorThemes[Object.keys(cursorThemes)[0]]);
