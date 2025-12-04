0017af4ed608a6c4d166ef788b78545ef11f601f
Aziz Rahmad <azayrahmad@gmail.com>
2025-12-04 11:26:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   assetsInclude: ["**/*.ani"],
-  base: "./",
+  base: "/azos-second-edition/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
