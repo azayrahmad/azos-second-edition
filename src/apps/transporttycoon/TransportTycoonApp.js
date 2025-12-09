8a821e7c6fe26a77d5404935e795468876fedc2d
Aziz Rahmad <azayrahmad@gmail.com>
2025-12-09 22:36:00 +0800

    fix window

--- src/apps/transporttycoon/TransportTycoonApp.js
+++ src/apps/transporttycoon/TransportTycoonApp.js
@@ -1,20 +1,24 @@
-import { Application } from '../Application.js';
+import { Application } from "../Application.js";
 
 export class TransportTycoonApp extends Application {
   _createWindow() {
     const win = new $Window({
       title: this.config.title,
-      icon: this.config.icon,
-      width: this.config.width,
-      height: this.config.height,
+      icons: this.config.icon,
+      outerWidth: this.config.width,
+      outerHeight: this.config.height,
       id: this.config.id,
+      resizable: true,
+      maximizable: true,
+      minimizable: true,
+      closable: true,
     });
 
-    const iframe = document.createElement('iframe');
-    iframe.src = 'https://atalbayrak.github.io/openttd/';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none';
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://atalbayrak.github.io/openttd/";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
 
     win.$content.append(iframe);
 
