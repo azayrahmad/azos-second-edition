58ccedf66e4484487e7b77bec374074f5c62b981
Aziz Rahmad <azayrahmad@gmail.com>
2025-12-22 13:43:26 +0800

    update stylings

--- src/apps/notepad-new/notepad-theme.js
+++ src/apps/notepad-new/notepad-theme.js
@@ -1,45 +1,29 @@
-import { EditorView } from '@codemirror/view';
+import { EditorView } from "@codemirror/view";
 
-export const notepadTheme = EditorView.theme({
-  '&': {
-    color: 'black',
-    backgroundColor: 'white',
-    fontFamily: "'Fixedsys Excelsior 3.01', monospace",
-    fontSize: '16px',
+export const notepadTheme = EditorView.theme(
+  {
+    "&": {
+      color: "black",
+      backgroundColor: "white",
+      fontFamily: '"Fixedsys Excelsior", monospace',
+      fontSize: "16px",
+    },
+    ".cm-content": {
+      caretColor: "#000",
+    },
+    ".cm-cursor, .cm-dropCursor": {
+      borderLeftColor: "#000",
+    },
+    "&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection":
+      {
+        backgroundColor: "var(--Hilight) !important",
+        color: "var(--HilightText) !important",
+      },
+    ".cm-gutters": {
+      backgroundColor: "#fff",
+      color: "#000",
+      border: "none",
+    },
   },
-  '.cm-content': {
-    caretColor: '#000',
-  },
-  '.cm-cursor, .cm-dropCursor': {
-    borderLeftColor: '#000',
-  },
-  '&.cm-focused .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection': {
-    backgroundColor: '#00f !important',
-    color: '#fff !important',
-  },
-  '.cm-gutters': {
-    backgroundColor: '#fff',
-    color: '#000',
-    border: 'none',
-  },
-  '.cm-scroller': {
-    scrollbarWidth: 'thin',
-    scrollbarColor: 'var(--button-face) var(--button-face)',
-  },
-  '.cm-scroller::-webkit-scrollbar': {
-    width: '16px',
-    height: '16px',
-  },
-  '.cm-scroller::-webkit-scrollbar-track': {
-    background: 'var(--button-face)',
-  },
-  '.cm-scroller::-webkit-scrollbar-thumb': {
-    background: 'var(--button-face)',
-    border: '1px solid',
-    borderColor: 'var(--button-hilight) var(--button-shadow) var(--button-shadow) var(--button-hilight)',
-    boxShadow: 'inset 1px 1px 0 0 var(--button-shadow), inset -1px -1px 0 0 var(--button-hilight)',
-  },
-  '.cm-scroller::-webkit-scrollbar-corner': {
-    backgroundColor: 'var(--button-face)',
-  },
-}, { dark: false });
+  { dark: false },
+);
