8be0fc0c437c0e9eb8c6d07c0548b708f17b7cd8
Aziz Rahmad <azayrahmad@gmail.com>
2026-01-13 09:33:54 +0800

    update reverse order

--- src/apps/klondike/Game.js
+++ src/apps/klondike/Game.js
@@ -3,7 +3,11 @@ import { TableauPile } from "./TableauPile.js";
 import { StockPile } from "./StockPile.js";
 import { FoundationPile } from "./FoundationPile.js";
 import { WastePile } from "./WastePile.js";
-import { getItem, setItem, LOCAL_STORAGE_KEYS } from "../../utils/localStorage.js";
+import {
+  getItem,
+  setItem,
+  LOCAL_STORAGE_KEYS,
+} from "../../utils/localStorage.js";
 
 export class Game {
   constructor() {
@@ -11,7 +15,7 @@ export class Game {
   }
 
   destroy() {
-    this.allCards.forEach(card => card.destroy());
+    this.allCards.forEach((card) => card.destroy());
   }
 
   initializeGame() {
@@ -22,26 +26,43 @@ export class Game {
     this.onScoreUpdate = () => {}; // Callback to notify UI of score changes
 
     const suits = ["♥", "♦", "♠", "♣"];
-    const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const ranks = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
     this.allCards = [];
     let fullDeck = [];
     for (const suit of suits) {
-        for (const rank of ranks) {
-            const card = new Card(suit, rank, this.cardBack);
-            fullDeck.push(card);
-            this.allCards.push(card);
-        }
+      for (const rank of ranks) {
+        const card = new Card(suit, rank, this.cardBack);
+        fullDeck.push(card);
+        this.allCards.push(card);
+      }
     }
 
     for (let i = fullDeck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [fullDeck[i], fullDeck[j]] = [fullDeck[j], fullDeck[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [fullDeck[i], fullDeck[j]] = [fullDeck[j], fullDeck[i]];
     }
 
     this.deck = { cards: fullDeck };
 
     this.tableauPiles = Array.from({ length: 7 }, () => new TableauPile());
-    this.foundationPiles = Array.from({ length: 4 }, () => new FoundationPile());
+    this.foundationPiles = Array.from(
+      { length: 4 },
+      () => new FoundationPile(),
+    );
     this.wastePile = new WastePile();
     this.drawnCards = [];
 
@@ -71,9 +92,9 @@ export class Game {
       this.drawnCards = [];
 
       // Then, deal new cards.
-      if (this.drawOption === 'three') {
+      if (this.drawOption === "three") {
         const cardsToDeal = this.stockPile.deal(3);
-        cardsToDeal.forEach(card => (card.faceUp = true));
+        cardsToDeal.forEach((card) => (card.faceUp = true));
         this.drawnCards = cardsToDeal;
       } else {
         const card = this.stockPile.deal();
@@ -85,13 +106,13 @@ export class Game {
     } else if (this.wastePile.cards.length > 0 || this.drawnCards.length > 0) {
       // Recycle: take cards from both waste and drawn piles
       const recycledFromWaste = this.wastePile.reset();
-      const recycledFromDrawn = this.drawnCards;
+      const recycledFromDrawn = this.drawnCards.reverse();
       this.drawnCards = [];
 
       // Combine recycled cards to form the new stock pile
-      const allRecycledCards = [...recycledFromWaste, ...recycledFromDrawn];
+      const allRecycledCards = [...recycledFromDrawn, ...recycledFromWaste];
       this.stockPile.cards = allRecycledCards;
-      this.stockPile.cards.forEach(card => card.faceUp = false);
+      this.stockPile.cards.forEach((card) => (card.faceUp = false));
     }
   }
 
@@ -105,44 +126,52 @@ export class Game {
   }
 
   flipTableauCard(pileIndex, cardIndex) {
-      const pile = this.tableauPiles[pileIndex];
-      if (pile && cardIndex === pile.cards.length - 1) {
-          if (pile.flipTopCard()) {
-            this.updateScore(5);
-          }
-          this.previousState = null;
+    const pile = this.tableauPiles[pileIndex];
+    if (pile && cardIndex === pile.cards.length - 1) {
+      if (pile.flipTopCard()) {
+        this.updateScore(5);
       }
+      this.previousState = null;
+    }
   }
 
   isValidMoveStack(pileType, pileIndex, cardIndex) {
-      if (pileType === 'tableau') {
-          const pile = this.tableauPiles[pileIndex];
-          if (!pile || cardIndex < 0 || cardIndex >= pile.cards.length) return false;
-          return pile.cards[cardIndex].faceUp;
-      }
-      if (pileType === 'drawn') {
-        return this.drawnCards.length > 0 && cardIndex === this.drawnCards.length - 1;
-      }
-      if (pileType === 'waste') {
-        return this.drawnCards.length === 0 && this.wastePile.cards.length > 0 && cardIndex === this.wastePile.cards.length - 1;
-      }
-      if (pileType === 'foundation') {
-          const pile = this.foundationPiles[pileIndex];
-          return cardIndex === pile.cards.length - 1;
-      }
-      return false;
+    if (pileType === "tableau") {
+      const pile = this.tableauPiles[pileIndex];
+      if (!pile || cardIndex < 0 || cardIndex >= pile.cards.length)
+        return false;
+      return pile.cards[cardIndex].faceUp;
+    }
+    if (pileType === "drawn") {
+      return (
+        this.drawnCards.length > 0 && cardIndex === this.drawnCards.length - 1
+      );
+    }
+    if (pileType === "waste") {
+      return (
+        this.drawnCards.length === 0 &&
+        this.wastePile.cards.length > 0 &&
+        cardIndex === this.wastePile.cards.length - 1
+      );
+    }
+    if (pileType === "foundation") {
+      const pile = this.foundationPiles[pileIndex];
+      return cardIndex === pile.cards.length - 1;
+    }
+    return false;
   }
 
   moveCards(fromPileType, fromPileIndex, cardIndex, toPileType, toPileIndex) {
     let fromPile;
-    if (fromPileType === 'tableau') fromPile = this.tableauPiles[fromPileIndex];
-    if (fromPileType === 'drawn') fromPile = { cards: this.drawnCards };
-    if (fromPileType === 'waste') fromPile = this.wastePile;
-    if (fromPileType === 'foundation') fromPile = this.foundationPiles[fromPileIndex];
+    if (fromPileType === "tableau") fromPile = this.tableauPiles[fromPileIndex];
+    if (fromPileType === "drawn") fromPile = { cards: this.drawnCards };
+    if (fromPileType === "waste") fromPile = this.wastePile;
+    if (fromPileType === "foundation")
+      fromPile = this.foundationPiles[fromPileIndex];
 
     let toPile;
-    if (toPileType === 'tableau') toPile = this.tableauPiles[toPileIndex];
-    if (toPileType === 'foundation') toPile = this.foundationPiles[toPileIndex];
+    if (toPileType === "tableau") toPile = this.tableauPiles[toPileIndex];
+    if (toPileType === "foundation") toPile = this.foundationPiles[toPileIndex];
 
     if (!fromPile || !toPile) return false;
 
@@ -153,7 +182,7 @@ export class Game {
       fromPile.cards.splice(cardIndex);
       toPile.cards.push(...cardsToMove);
 
-      if (fromPileType === 'drawn') {
+      if (fromPileType === "drawn") {
         this.refillDrawnCardsFromWaste();
       }
 
@@ -188,7 +217,7 @@ export class Game {
   setCardBack(cardBack) {
     this.cardBack = cardBack;
     setItem(LOCAL_STORAGE_KEYS.klondikeCardBack, cardBack);
-    this.allCards.forEach(card => card.setCardBack(cardBack));
+    this.allCards.forEach((card) => card.setCardBack(cardBack));
   }
 
   setDrawOption(option) {
@@ -200,9 +229,9 @@ export class Game {
       stockPileCards: [...this.stockPile.cards],
       wastePileCards: [...this.wastePile.cards],
       drawnCards: [...this.drawnCards],
-      tableauPilesCards: this.tableauPiles.map(p => [...p.cards]),
-      foundationPilesCards: this.foundationPiles.map(p => [...p.cards]),
-      allCardsFaceUp: this.allCards.map(c => c.faceUp)
+      tableauPilesCards: this.tableauPiles.map((p) => [...p.cards]),
+      foundationPilesCards: this.foundationPiles.map((p) => [...p.cards]),
+      allCardsFaceUp: this.allCards.map((c) => c.faceUp),
     };
   }
 
@@ -222,7 +251,7 @@ export class Game {
     });
 
     this.allCards.forEach((card, index) => {
-        card.faceUp = this.previousState.allCardsFaceUp[index];
+      card.faceUp = this.previousState.allCardsFaceUp[index];
     });
 
     this.previousState = null;
