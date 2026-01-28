e191fe9eb05611c104bf8baa16e58a69d16576f7
Aziz Rahmad <azayrahmad@gmail.com>
2026-01-28 13:30:09 +0800

    add multiple files icon and update properties filename

--- src/config/icons.js
+++ src/config/icons.js
@@ -251,6 +251,10 @@ export const ICONS = {
     16: new URL("../assets/icons/file_lines-1.png", import.meta.url).href,
     32: new URL("../assets/icons/file_lines-0.png", import.meta.url).href,
   },
+  fileSet: {
+    16: new URL("../assets/icons/file_set-1.png", import.meta.url).href,
+    32: new URL("../assets/icons/file_set-0.png", import.meta.url).href,
+  },
   "cursor-explorer": {
     16: new URL("../assets/icons/COMCTL32_20481-16.png", import.meta.url).href,
     32: new URL("../assets/icons/COMCTL32_20481-32.png", import.meta.url).href,
